11d38667b3d927e070fce38069cc6973bd3d0300
anreall anreall <anreall2000@gmail.com>
2023-04-16 18:02:34 +0300

    add parsers

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,5 @@
 import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
+import retriveObjectFromFile from './parsers.js';
 
 const firstObjectOnlyKeys = (o1, o2) => {
   const keys = Object.keys(o1).filter((el) => !Object.keys(o2).includes(el));
@@ -30,30 +28,6 @@ const genDiff = (o1, o2) => {
     return [...diff, `- ${key}: ${o1[key]}`, `+ ${key}: ${o2[key]}`];
   }, []);
   return `{\n  ${diffOutputArr.join('\n  ')}\n}`;
-};
-
-const getFileType = (fileName) => {
-  if (fileName.endsWith('.yml') || fileName.endsWith('.yaml')) {
-    return 'yaml';
-  }
-  if (fileName.endsWith('.json')) {
-    return 'json';
-  }
-  return '';
-};
-
-const retriveObjectFromFile = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath);
-  const type = getFileType(filepath);
-  const content = fs.readFileSync(absolutePath, 'utf8');
-  switch (type) {
-    case 'json':
-      return JSON.parse(content);
-    case 'yaml':
-      return yaml.load(content);
-    default:
-      throw new Error('Unknown file type');
-  }
 };
 
 const genDiffFiles = (filepath1, filepath2) => {
