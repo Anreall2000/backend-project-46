be0bb8fadcdb1d72e8450e6187993f9c597af46b
anreall anreall <anreall2000@gmail.com>
2023-04-16 16:54:34 +0300

    refactor gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,13 +2,19 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
+const firstObjectOnlyKeys = (o1, o2) => {
+  const keys = Object.keys(o1).filter((el) => !Object.keys(o2).includes(el));
+  return new Set(keys);
+};
+
 const genDiff = (o1, o2) => {
   const allKeys = _.sortBy([...new Set([...Object.keys(o1), ...Object.keys(o2)])], (el) => el);
-  const obj1OnlyKeys = new Set(Object.keys(o1).filter((el) => !Object.keys(o2).includes(el)));
-  const obj2OnlyKeys = new Set(Object.keys(o2).filter((el) => !Object.keys(o1).includes(el)));
+
+  const obj1OnlyKeys = firstObjectOnlyKeys(o1, o2);
+  const obj2OnlyKeys = firstObjectOnlyKeys(o2, o1);
+
   const mutualKeys = Object.keys(o1).filter((el) => Object.keys(o2).includes(el));
   const sameValueKeys = new Set(mutualKeys.filter((el) => o1[el] === o2[el]));
-  const differentValueKeys = new Set(mutualKeys.filter((el) => o1[el] !== o2[el]));
 
   const diffOutputArr = allKeys.reduce((diff, key) => {
     if (obj1OnlyKeys.has(key)) {
@@ -20,10 +26,7 @@ const genDiff = (o1, o2) => {
     if (sameValueKeys.has(key)) {
       return [...diff, `  ${key}: ${o1[key]}`];
     }
-    if (differentValueKeys.has(key)) {
-      return [...diff, `- ${key}: ${o1[key]}`, `+ ${key}: ${o2[key]}`];
-    }
-    return diff;
+    return [...diff, `- ${key}: ${o1[key]}`, `+ ${key}: ${o2[key]}`];
   }, []);
   return `{\n  ${diffOutputArr.join('\n  ')}\n}`;
 };
