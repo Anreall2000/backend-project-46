29127bb2d804a2b79f316323084684714e37592d
anreall anreall <anreall2000@gmail.com>
2023-04-17 20:31:04 +0300

    fix import

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import genDiffFiles from '../src/formatters';
+import genDiffFiles from '../src/formatters/index.js';
 
 program.name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => genDiffFiles(filepath1, filepath2, options.format));
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiffFiles(filepath1, filepath2, options.format));
+  });
 
 program.parse();
